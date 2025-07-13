a1339fb56921213408e027c8448a0fe657d05c93
LJThao <yengfong.thao@atlasschool.com>
2025-07-13 17:08:33 -0500

    createdir func

---
+++ hadoop_project/4-createdir.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2.7
+"""HDFS with Python (Snakebite) Module"""
+from snakebite.client import Client
+
+
+def createdir(l):
+    """Function that creates the directories listed on l within HDFS"""
+    client = Client('localhost', 9000)
+    output = client.mkdir(l, create_parent=True)
+    for entry in output:
+        print(entry)
